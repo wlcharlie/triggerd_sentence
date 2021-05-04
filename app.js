da3c91dd736f8be597ad2059a564319badc3a977
wlcharlie <wl00606352@gmail.com>
2021-05-04 16:00:03 +0800

    problem of handlebars

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const bodyParser = require("body-parser")
 const wordPackage = require("./sentence")
+const hbsHelpers = require('handlebars-helpers')
 const app = express()
+const helpers = hbsHelpers()
 
 // 正在研究helper中...，除了保留使用者點選過的資料功能還未做出QQ
 app.engine("hbs", exphbs({
-  helpers: {
-    ifEqual: function (a, b) {
-      if (a === b) {
-        return "checked"
-      }
-    }
-  }, defaultLayout: 'main', extname: '.hbs'
+  helpers: helpers, defaultLayout: 'main', extname: '.hbs'
 }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -25,6 +21,7 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const options = req.body
   const sentence = wordPackage(options.character)
+  console.log(options, sentence)
   res.render('index', { options, sentence })
 })
 
